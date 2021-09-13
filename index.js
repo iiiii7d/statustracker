98071d2f51123b05a129c785bba7ec7db2acb437
7d <61975820+iiiii7d@users.noreply.github.com>
2021-09-13 23:03:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,170 @@
+var http = require('http'); 
+var url = require('url'); 
+var fs = require('fs'); 
+var { parse } = require('querystring'); 
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var cheerio = require('cheerio');
+
+var staffs = [
+  "Frumple",
+  "Chiefbozx",
+  "AP_Red",
+  "Cynra_",
+  "sesese9",
+  "Tom_Pairs", 
+  "Skelezomperman",
+  "Needn_NL",
+  "Missa_Solemnis",
+  "ondist",
+  "MPolo455",
+  "hvt2011",
+  "Narnia17",
+  "MC_Protocol",
+  "DintyB"];
+
+var server = http.createServer(function(request, response) {  
+  if (request.method === 'POST') {
+    let body = '';
+    request.on('data', chunk => {
+      body += chunk.toString(); // convert Buffer to string
+    });
+    request.on('end', () => {
+      console.log(parse(body));
+      response.end("complete");
+    });
+  }
+  else {
+    var path = url.parse(request.url).pathname; 
+    switch (path) {  
+      case '/':
+        fs.readFile(__dirname + '/index.html', function(error, data) { 
+          if (error) { 
+            response.writeHead(404); 
+            response.write(error); 
+            response.end(); 
+          } else { 
+            response.writeHead(200, { 
+              'Content-Type': 'text/html' 
+            }); 
+            response.write(data); 
+            response.end(); 
+          } 
+        }); 
+        break;
+      case '/index.js': 
+      case '/script.js': 
+        fs.readFile(__dirname + path, function(error, data) { 
+          if (error) { 
+            response.writeHead(404); 
+            response.write(error); 
+            response.end(); 
+          } else { 
+            response.writeHead(200, { 
+              'Content-Type': 'application/javascript' 
+            }); 
+            response.write(data); 
+            response.end(); 
+          } 
+        }); 
+        break;
+      case '/count.json': 
+        fs.readFile('count.json', function(error, data) { 
+          if (error) { 
+            response.writeHead(404); 
+            response.write(error); 
+            response.end(); 
+          } else { 
+            response.writeHead(200, { 
+              'Content-Type': 'application/json' 
+            }); 
+            response.write(data); 
+            response.end(); 
+          } 
+        }); 
+        break;
+      default: 
+        response.writeHead(404); 
+        response.write("404"); 
+        response.end(); 
+        break; 
+    }
+  } 
+}); 
+server.listen(8082);
+
+function update()
+{
+  source = null;
+  var request = new XMLHttpRequest();
+  request.open("GET", "https://status.minecartrapidtransit.net", true);
+  request.send(null);
+  request.onreadystatechange = function() {
+    if (request.readyState == 4)
+    {
+      source = request.responseText;
+      const $ = cheerio.load(source);
+      trs = $(".name");
+      
+      timestamp = Math.round(new Date() / 1000 / 60).toString();
+      entry = {
+        "Main": {}, "Mumble": {}
+      };
+
+      for (i = 0; i < trs.length; i++)
+      {
+        if (!["Main", "Mumble"].includes(trs.eq(i).text())) continue;
+        
+        players = trs.eq(i).parents("tr").eq(0).children(".players");
+        names = [];
+        if (players.children("ul").length != 0)
+        {
+          list = players.children("ul").children("li")
+          for (j = 0; j < list.length; j++) 
+          {
+            names.push(list.eq(j).text());
+          }
+        }
+
+        entry[trs.eq(i).text()] = {
+          "players": names.length
+        }
+
+        function numOfStaff(list)
+        {
+          staff = 0
+          for (k = 0; k < list.length; k++)
+          {
+            if (staffs.includes(list[k])) staff++;
+          }
+          return staff;
+        }
+
+        if (trs.eq(i).text() == "Main") entry[trs.eq(i).text()]["staffs"] = numOfStaff(names);
+
+      }
+
+      stringentry = `${entry["Main"]["players"]} ${entry["Main"]["staffs"]} ${entry["Mumble"]["players"]}`
+      console.log(entry)
+      fs.readFile('count.json', (err, data) => {
+        if (err) throw err;
+        let logs = JSON.parse(data);
+        logs[timestamp] = stringentry;
+        if (Object.keys(logs).length > 600)
+        {
+          var temp = Object.keys(logs);
+          temp.sort((a, b) => a - b);
+          console.log(temp[0]);
+          delete logs[temp[0]];
+        }
+        //console.log(logs);
+        newdata = JSON.stringify(logs, null, 2);
+        fs.writeFile('count.json', newdata, (err) => {
+          if (err) throw err;
+          //console.log('Data written to file');
+        });
+      });
+    }
+  };
+}
+update();
+setInterval(update, 60*1000);
---
+++ script.js
@@ -0,0 +1,97 @@
+result = {};
+
+window.onload = function() {
+  d = new Date();
+  document.getElementById("end").value = `${extraZero(d.getFullYear())}-${extraZero(d.getMonth()+1)}-${extraZero(d.getDate())}T${extraZero(d.getHours())}:${extraZero(d.getMinutes())}`;
+  start = Date.parse(document.getElementById("start").value) / 60 / 1000;
+  end = Date.parse(document.getElementById("end").value) / 60 / 1000;
+  $.getJSON("count.json", (r) => {result = r; chart(result, start, end);});
+};
+
+function update()
+{
+  start = Date.parse(document.getElementById("start").value) / 60 / 1000;
+  end = Date.parse(document.getElementById("end").value) / 60 / 1000;
+  chart(result, start, end);
+}
+
+function extraZero(n)
+{
+  if (n.toString().length == 1) return "0".concat(n.toString());
+  else return n.toString();
+}
+
+function view(mins)
+{
+  de = new Date();
+  document.getElementById("end").value = `${extraZero(de.getFullYear())}-${extraZero(de.getMonth()+1)}-${extraZero(de.getDate())}T${extraZero(de.getHours())}:${extraZero(de.getMinutes())}`;
+  if (mins == null) document.getElementById("start").value = "2020-11-01T00:00";
+  else
+  {
+    ds = new Date();
+    ds.setMinutes(ds.getMinutes() - mins);
+    document.getElementById("start").value = `${extraZero(ds.getFullYear())}-${extraZero(ds.getMonth()+1)}-${extraZero(ds.getDate())}T${extraZero(ds.getHours())}:${extraZero(ds.getMinutes())}`;
+  }
+  update();
+}
+
+function chart(result, start, end)
+{
+  mcplayers = [];
+  mcstaffs = [];
+  mumble = [];
+  for (const [time, val] of Object.entries(result))
+  {
+    if (time >= start && time <= end)
+    {
+      mcplayers.push({x: new Date(time * 60 * 1000), y: parseInt(val.split(" ")[0])});
+      mcstaffs.push({x: new Date(time * 60 * 1000), y: parseInt(val.split(" ")[1])});
+      mumble.push({x: new Date(time * 60 * 1000), y: parseInt(val.split(" ")[2])});
+    }
+  }
+
+  var chart = new CanvasJS.Chart("chart", {
+    title: {text: "Players"},
+    zoomEnabled: true,
+    toolTip: {
+      shared: true,
+      contentFormatter: function(e){
+        var str = e.entries[0].dataPoint.x.toLocaleString() + "<br>";
+        cplayers = 0;
+        cstaffs = 0;
+        for (var i = 0; i < e.entries.length; i++) {
+          var temp = `<span style="color:${e.entries[i].dataSeries.color};">${e.entries[i].dataSeries.name} </span><strong>${e.entries[i].dataPoint.y}</strong><br/>` ; 
+          str = str.concat(temp);
+          if (e.entries[i].dataSeries.name == "players") cplayers = e.entries[i].dataPoint.y;
+          if (e.entries[i].dataSeries.name == "staffers") cstaffs = e.entries[i].dataPoint.y;
+        }
+        var pc = Math.round(cstaffs / cplayers * 100);
+        if (pc.isNaN) pc = "-";
+        str = str.concat(`% staff: ${pc}`);
+        return (str);
+      }
+    },
+    data: [
+      {        
+        type: "line",
+        name: "players",
+        color: "darkgreen",
+        dataPoints: mcplayers
+      },
+      {        
+        type: "line",
+        name: "staffers",
+        color: "#cc0000",
+        dataPoints: mcstaffs
+      },
+      {        
+        type: "line",
+        name: "mumble",
+        color: "#87ceeb",
+        dataPoints: mumble
+      }
+    ]
+  });
+
+  chart.render();
+}
